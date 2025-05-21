38dd0111e630b550c9cec69228a9421d68c7ec4b
koutakagatsume <kouta.kagatsume@east.ntt.co.jp>
2025-05-21 15:21:21 +0900

    create

--- todo/todoproject/todoapp/urls.py
+++ todo/todoproject/todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('', TaskList.as_view()),
+    path('', TaskList.as_view(), name='task_list'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
+    path('task/create/', TaskCreate.as_view(), name='task_create'),
 ]
--- todo/todoproject/todoapp/views.py
+++ todo/todoproject/todoapp/views.py
@@ -1,15 +1,28 @@
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
-# from .models import Task
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 
 from .models import Task
 
 # Create your views here.
+
+# タスク一覧
 class TaskList(ListView):
     model = Task
     print(model)
     context_object_name = 'tasks'
 
+# タスク詳細
 class TaskDetail(DetailView):
     model = Task
+
+# タスク作成
+class TaskCreate(CreateView):
+    model = Task
+    # 全てのフィールド
+    fields = "__all__"
+    success_url = reverse_lazy('task_list')
+
+# タスク更新
+
